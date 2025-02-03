b4b21e6af1394b298594038fdb3ea0875dd950d3
Rickson Rocha <ricksonroccha21@gmail.com>
2025-02-03 10:11:19 -0300

    [feat]:creates  entity User.java

---
+++ src/main/java/com/rsolution/rcommerce/domain/User.java
@@ -0,0 +1,93 @@
+package com.rsolution.rcommerce.domain;
+
+import com.rsolution.rcommerce.domain.order.Order;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String password;
+
+    private String email;
+
+    private String phone;
+
+    private LocalDate birthday;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> order = new ArrayList<>();
+
+
+    public User() {}
+
+    public User(Long id, String name, String password, String email, String phone, LocalDate birthday) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.birthday = birthday;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+    public List<Order> getOrder() {
+        return order;
+    }
+}

9ca049dccbbc481718cb4ecc38a1f19b87ba47f9
Rickson Rocha <ricksonroccha21@gmail.com>
2025-02-03 09:05:21 -0300

    first commit

---
+++ src/main/java/com/rsolution/rcommerce/RcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.rsolution.rcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsolution/rcommerce/RcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsolution.rcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
